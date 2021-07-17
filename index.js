5de03510b1f18ced41b19fa412667c0125ff0cfc
BeSoEasy <besoeasy@users.noreply.github.com>
2021-07-17 08:41:38 +0530

    changed node + sanitize

--- index.js
+++ index.js
@@ -2,39 +2,40 @@ const ENS = require('ethereum-ens');
 const Web3 = require('web3');
 const fs = require('fs');
 
-const provider = new Web3.providers.HttpProvider(`https://main-light.eth.linkpool.io`);
+const provider = new Web3.providers.HttpProvider(`https://cloudflare-eth.com`);
 const ens = new ENS(provider);
 
-fs.readFile('list.txt', async function (error, data) {
-	if (error) {
-		throw error;
-	}
-	
-	try {
 
-			data
-		.toString()
-		.split('\n')
-		.forEach(async function (line, index, arr) {
-			if (index === arr.length - 1 && line === '') {
-				return;
+
+function sleep(ms) {
+	second = ms * 1000;
+
+	return new Promise((resolve) => setTimeout(resolve, second));
+}
+
+function filecontenttoarray(file) {
+	var file = fs.readFileSync(file, 'utf8');
+	var array = file.split('\n');
+	return array;
+}
+
+async function main() {
+	lines = await filecontenttoarray('list.txt');
+
+	for (let i = 0; i < lines.length; i++) {
+		// sanitize
+		line = lines[i].trim() + `.eth`;
+
+		// checking if domain is already registered
+		await ens.owner(line).then(function (response) {
+			if (response == '0x0000000000000000000000000000000000000000') {
+				console.log(line + ' is not owned');
 			}
-
-			await ens.owner(line.trim() + `.eth`).then(function (response) {
-				if (response == '0x0000000000000000000000000000000000000000') {
-					console.log(line);
-				}
-			});
 		});
-		
-		
-}
-catch(err) {
-console.log(err)
-}
-	
-	
-	
 
+		// sleeping for 15 seconds to avoid rate limit
+		await sleep(15);
+	}
+}
 
-});
+main();
