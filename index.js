a89aa663504336bb0975fb994abbf1b76500c827
BeSoEasy <besoeasy@users.noreply.github.com>
2022-02-11 17:50:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const fs = require('fs');
 const { ethers } = require('ethers');
 const provider = new ethers.providers.getDefaultProvider('mainnet');
 
-function sleep(ms) {
-	second = ms * 1000;
-	return new Promise((resolve) => setTimeout(resolve, second));
-}
-
 function filecontenttoarray(file) {
 	var file = fs.readFileSync(file, 'utf8');
 	var array = file.split('\n');
@@ -17,15 +12,14 @@ async function main() {
 	lines = await filecontenttoarray('list.txt');
 
 	for (let i = 0; i < lines.length; i++) {
-		line = lines[i].trim() + `.eth`;
+		var line = lines[i].trim() + `.eth`;
 
 		var address = await provider.resolveName(line);
 
 		if (!address) {
 			console.log(line + ' is not owned');
 		}
-
-		await sleep(2);
+		
 	}
 }
 
